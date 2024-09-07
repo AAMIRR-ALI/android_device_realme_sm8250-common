996f0f7a844ec131612f0ea5a6ca170be840ad3e
Blackmanx <blackman@sajl.tk>
2024-09-07 12:52:47 +0200

    sm8250-common: BoardConfigCommon: Bring back memcg

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -87,7 +87,7 @@ TARGET_USES_ION := true
 TARGET_USES_QCOM_DISPLAY_BSP := true
 TARGET_USES_QTI_MAPPER_2_0 := true
 TARGET_USES_QTI_MAPPER_EXTENSIONS_1_1 := true
-TARGET_SCREEN_DENSITY := 480
+TARGET_SCREEN_DENSITY := 397
 TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE := true
 
 # Filesystem
@@ -101,6 +101,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/device_framework_matrix.xml \
     $(COMMON_PATH)/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
+
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 
@@ -119,6 +120,7 @@ BOARD_KERNEL_CMDLINE := \
     androidboot.hardware=qcom \
     androidboot.usbcontroller=a600000.dwc3 \
     cgroup.memory=nokmem,nosocket \
+    androidboot.memcg=1 \
     earlycon=msm_geni_serial,0xa90000 \
     kpti=off \
     loop.max_part=7 \
